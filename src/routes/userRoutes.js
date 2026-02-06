af793e3d171164c7cffe58598c8d3dd4ffaa5dfb
dhirajpandit <dhirajpandit5050@gmail.com>
2026-02-06 16:38:02 +0530

    feat: setup user routes

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { UserController } from '../controllers/UserController.js';
+import { protect, admin } from '../middlewares/authMiddleware.js';
+
+const router = express.Router();
+const userController = new UserController();
+
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.get('/profile', protect, userController.getProfile);
+router.get('/', protect, admin, userController.getAllUsers);
+
+export default router;
