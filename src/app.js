25ab738bcc521aeb8a049de5659f90106efd948d
dhirajpandit <dhirajpandit5050@gmail.com>
2026-02-10 16:34:46 +0530

    feat: register product routes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { AppError } from './utils/AppError.js';
 import userRoutes from './routes/userRoutes.js';
+import productRoutes from './routes/productRoutes.js';
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
 
 // 404 Handler
 app.all('*', (req, res, next) => {
