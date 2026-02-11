caa35ebe1708b0b0797ad45431b78504e88ffab3
dhirajpandit <dhirajpandit5050@gmail.com>
2026-02-11 11:55:25 +0530

    feat: add order validation schema

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -21,6 +21,28 @@ export const productSchema = Joi.object({
     category: Joi.string().required(),
 });
 
+export const orderSchema = Joi.object({
+    orderItems: Joi.array().items(
+        Joi.object({
+            name: Joi.string().required(),
+            quantity: Joi.number().required(),
+            imageUrl: Joi.string().required(),
+            price: Joi.number().required(),
+            product: Joi.string().required(),
+        })
+    ).required(),
+    shippingAddress: Joi.object({
+        address: Joi.string().required(),
+        city: Joi.string().required(),
+        postalCode: Joi.string().required(),
+        country: Joi.string().required(),
+    }).required(),
+    paymentMethod: Joi.string().required(),
+    taxPrice: Joi.number().required(),
+    shippingPrice: Joi.number().required(),
+    totalPrice: Joi.number().required(),
+});
+
 export const validate = (schema, data) => {
     const { error } = schema.validate(data);
     if (error) {
